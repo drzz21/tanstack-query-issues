6264e824e9d33ddca97c5a7a12f02b52536e9100
Francisco Diosdado <drz.y2k@gmail.com>
2026-03-24 13:47:58 -0600

    Instalación - TanStack Query

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,17 @@ import { RouterProvider } from 'react-router-dom';
 import { router } from './router';
 
 import './index.css';
+//instalacion de react query y las devtools
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+	<React.StrictMode>
+		<QueryClientProvider client={queryClient}>
+			<RouterProvider router={router} />
+			<ReactQueryDevtools />
+		</QueryClientProvider>
+	</React.StrictMode>,
 );
