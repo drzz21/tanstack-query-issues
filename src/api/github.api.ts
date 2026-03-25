3b756b50c1dc9f0144e80be4125680f18433958a
Francisco Diosdado <drz.y2k@gmail.com>
2026-03-25 14:17:05 -0600

    Autenticación en GitHub Configuración de variable de entorno

--- src/api/github.api.ts
+++ src/api/github.api.ts
@@ -1,9 +1,14 @@
 //creamos la configuración de axios con la url base para las peticiones
 import axios from 'axios';
 
+// console.log(import.meta.env.VITE_GITHUB_TOKEN);
+
 export const githubApi = axios.create({
-  baseURL: 'https://api.github.com/repos/facebook/react',
-  headers:{
-	//TODO Api key de Github
-  }
+	baseURL: 'https://api.github.com/repos/facebook/react',
+	headers: {
+		//TODO Api key de Github
+    //Configuramos variable de api de Github en el archivo .env.local 
+    // y la importamos aquí para usarla en las peticiones
+		Authorization: `Bearer ${import.meta.env.VITE_GITHUB_TOKEN}`,
+	},
 });
--- src/issues/hooks/useLabels.tsx
+++ src/issues/hooks/useLabels.tsx
@@ -1,6 +1,6 @@
 import { useQuery } from '@tanstack/react-query';
 import { getLabels } from '../actions';
-import { GithubLabel } from '../interfaces';
+// import { GithubLabel } from '../interfaces';
 
 //cremos el hook que contendrá el query fn que usaremos para consultar los labels
 export const useLabels = () => {
@@ -12,28 +12,28 @@ export const useLabels = () => {
 		//con placeholder data asignamos los valores iniciales del query, 
 		// esto es útil para evitar el estado de loading y mostrar algo mientras se carga la data real, 
 		// en este caso mostramos dos labels de ejemplo
-		placeholderData: [
-			{
-				id: 1,
-				node_id: 'MDU6TGFiZWwx',
-				url: 'https://api.github.com/repos/facebook/react/labels/bug',
-				name: 'bug',
-				color: 'd73a4a',
-				default: false,
-				description: "Something isn't working",
-			} satisfies GithubLabel,
-			//con satisfies nos aseguramos de cumplir la estructura de la interfaz GithubLabel,
-			//  esto nos ayuda a evitar errores de tipo y a tener autocompletado en el editor
-			{
-				id: 791921801,
-				node_id: 'MDU6TGFiZWw3OTE5MjE4MDE=',
-				url: 'https://api.github.com/repos/facebook/react/labels/%E2%9D%A4%EF%B8%8F',
-				name: '❤️',
-				color: 'ffffff',
-				default: false,
-				'description': '123',
-			} satisfies GithubLabel,
-		],
+		// placeholderData: [
+		// 	{
+		// 		id: 1,
+		// 		node_id: 'MDU6TGFiZWwx',
+		// 		url: 'https://api.github.com/repos/facebook/react/labels/bug',
+		// 		name: 'bug',
+		// 		color: 'd73a4a',
+		// 		default: false,
+		// 		description: "Something isn't working",
+		// 	} satisfies GithubLabel,
+		// 	//con satisfies nos aseguramos de cumplir la estructura de la interfaz GithubLabel,
+		// 	//  esto nos ayuda a evitar errores de tipo y a tener autocompletado en el editor
+		// 	{
+		// 		id: 791921801,
+		// 		node_id: 'MDU6TGFiZWw3OTE5MjE4MDE=',
+		// 		url: 'https://api.github.com/repos/facebook/react/labels/%E2%9D%A4%EF%B8%8F',
+		// 		name: '❤️',
+		// 		color: 'ffffff',
+		// 		default: false,
+		// 		'description': '123',
+		// 	} satisfies GithubLabel,
+		// ],
 		//initial data funciona un tanto diferente
 		//ya que este este es el valor que se asigna por primera vez y permanecerá
 		// hasta que se actualice con la data real, mientras que el placeholderData se muestra solo mientras se carga la data real,
